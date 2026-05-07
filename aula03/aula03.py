90be667bd9388d8ffc9812c8c622fd7f89b30413
jhonny.almeida <jhonny.almeida@aluno.senai.br>
2026-05-07 12:08:51 -0300

    Add files via upload

---
+++ aula03/aula03.py
@@ -0,0 +1,28 @@
+# Note: boletim 2.0
+
+import os
+
+os.system("cls")
+
+print("boletim escolar")
+
+lista_notas=[]
+nome=input("Digite o nome do aluno: ").title
+curso=input("Digite o curso: ").upper
+
+while True:
+ notas=input("Digite a sua nota: ")
+ nota=float(notas)
+ lista_notas.append(nota)
+ print(lista_nota)
+
+
+opcao= input("Deseja adicionar mais notas? (Enter-continua| n-Não)").lower()
+
+if opcao=="n":
+ brea
+media = sum(lista_notas)/len(lista_notas)
+
+print(media)
+
+
