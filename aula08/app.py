c1454461488f5fc78ac6355f70a0730eb8f7525d
jhonny.almeida <jhonny.almeida@aluno.senai.br>
2026-05-12 11:40:42 -0300

    Add files via upload

---
+++ aula08/app.py
@@ -0,0 +1,26 @@
+def funcao_segundo_grau(a, b, c):
+    print("ola mundo ")
+    return a, b, c 
+
+x = funcao_segundo_grau(1, 2, 3)
+print(x)
+
+# função com parametro 
+def funcao_sendo_grau(a, b, c):
+    
+    
+#chamando a variavel e chamando dentro da variavel
+
+ def soma(a,b):
+    resultado = a + b 
+    return resultado
+ 
+ num1 = int(input("digite um numero: "))
+ num2= int(input("digite outro numero: "))
+ 
+ def mostrar_msg():
+    print("olá mundo das funcões! ")
+
+mostrar_msg()
+
+
